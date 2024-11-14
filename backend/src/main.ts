475766ce16d8f537f70ec84c749e793d3b4e1122
21120185 <21120185@student.hcmus.edu.vn>
2024-11-14 10:14:30 +0700

    refactor: add API swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  const document = YAML.load('../backend/src/swagger/openapi.yaml');
+
+  // Set up Swagger
+  SwaggerModule.setup('api-docs', app, document);
+
   app.enableCors({
     origin: 'http://localhost:3000', // URL của Next.js frontend
     credentials: true,
--- backend/src/shared/strategies/jwt.strategy.ts
+++ backend/src/shared/strategies/jwt.strategy.ts
@@ -5,6 +5,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
