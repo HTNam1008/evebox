c40d15087ab38db8b306433df202168f1d435769
dttHuongg <huong.dinhtthuy203@gmail.com>
2024-12-04 23:55:48 +0700

    fea: complete flow of forgot and reset password

--- frontend/src/pages/[page].tsx
+++ frontend/src/pages/[page].tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Dashboard from './components/Dashboard';
 import Register from './components/Register';
 import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword'
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword'
+
 
 const DynamicPage = ({ page }: { page: string }) => {
   if (page === '/') {
@@ -16,6 +18,9 @@ const DynamicPage = ({ page }: { page: string }) => {
   }
   if (page === 'forgot-password') {
     return <ForgotPassword />;
+  }
+  if (page === 'reset-password') {
+    return <ResetPassword />;
   }
   return <div>Trang không tồn tại</div>;
 };
