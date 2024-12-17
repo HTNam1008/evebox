14f852bf58e5c24283ab8591be6f75f655d97ab3
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2024-12-17 19:52:25 +0700

    chore: migration prisma

--- backend/src/infrastructure/database/prisma/migrations/20241129083912_edit_model_otp/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `attempts` to the `otps` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "otps" ADD COLUMN     "attempts" INTEGER NOT NULL;
