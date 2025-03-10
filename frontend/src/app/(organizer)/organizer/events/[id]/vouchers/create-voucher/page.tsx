623ee7b4c4c018bc2d2261edd0f19b63ff7cddfc
dttHuongg <huong.dinhtthuy203@gmail.com>
2025-03-10 21:36:16 +0700

    fea: add voucher page

---
+++ frontend/src/app/(organizer)/organizer/events/[id]/vouchers/create-voucher/page.tsx
@@ -0,0 +1,30 @@
+// app/page.tsx
+"use client";
+import { useState } from "react";
+import CardSelection from "./components/cardSelection";
+import BasicInfo from "./components/basicInfo";
+import VoucherSettings from "./components/voucherSetting";
+import ApplyScope from "./components/applyScope";
+import SidebarOrganizer from "../../components/sidebarOrganizer";
+
+export default function Home() {
+    const [voucherType, setVoucherType] = useState("single");
+
+    return (
+        <main>
+            <div className="flex min-h-screen bg-gray-100">
+                <div className="w-64 bg-gray-900 text-white">
+                    <SidebarOrganizer />
+                </div>
+                <div className="flex-1 p-6">
+                    <div className="max-w-4xl mx-auto p-6 space-y-6">
+                        <CardSelection voucherType={voucherType} setVoucherType={setVoucherType} />
+                        <BasicInfo voucherType={voucherType} />
+                        <VoucherSettings voucherType={voucherType} />
+                        <ApplyScope />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
