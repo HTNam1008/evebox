e911b56f043e85b89511460ad3ffb22ad9e3877c
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-03-17 13:39:33 +0700

    Feat: Update Prisma

--- be-microservices/microservices/ticket-svc/src/infrastructure/database/prisma/migrations/20250309154041_add_quantity_to_ticket_type/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ShowingTicketTypeQuantity" (
-    "id" TEXT NOT NULL,
-    "showingId" TEXT NOT NULL,
-    "ticketTypeId" TEXT NOT NULL,
-
-    CONSTRAINT "ShowingTicketTypeQuantity_pkey" PRIMARY KEY ("id")
-);
