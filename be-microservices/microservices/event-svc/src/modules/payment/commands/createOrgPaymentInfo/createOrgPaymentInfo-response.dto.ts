eaea2ade7bcaa1821a23436131ff0e83677125e7
DatTruong0108 <dattantruong.work@gmail.com>
2025-03-28 15:52:33 +0700

    feat: API create org payment info

---
+++ be-microservices/microservices/event-svc/src/modules/payment/commands/createOrgPaymentInfo/createOrgPaymentInfo-response.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { BaseResponse } from 'src/shared/constants/baseResponse';
+
+export class CreateOrgPaymentInfoResponseDto extends BaseResponse {
+  @ApiProperty({ example: 'd1f6f89e-5b77-4e57-9f1a-123456789abc', description: 'Created OrgPaymentInfo id' })
+  data: {
+    id: string;
+    organizerId: string;
+    accountName: string;
+    accountNumber: string;
+    bankName: string;
+    branch: string;
+    businessType: number;
+    fullName?: string;
+    address?: string;
+    taxCode?: string;
+  }
+}
