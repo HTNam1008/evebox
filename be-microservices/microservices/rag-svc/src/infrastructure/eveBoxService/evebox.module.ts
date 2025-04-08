3d9931aecff399f1dfdc2f170fd2b9710087343d
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-04-09 01:30:12 +0700

    Init: SVC

---
+++ be-microservices/microservices/rag-svc/src/infrastructure/eveBoxService/evebox.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+// import { EveboxService } from './evebox.service';
+// import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [],
+  providers: [],
+  exports: [],
+})
+export class EveboxModule {}
