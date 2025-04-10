87d178cf47307a95a3708e6ba5b571d433ffd8dc
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-04-10 16:14:33 +0700

    Feat: Edit prisma to use one time only

--- be-microservices/microservices/rag-svc/src/infrastructure/database/prisma/prisma.service.ts
+++ be-microservices/microservices/rag-svc/src/infrastructure/database/prisma/prisma.service.ts
@@ -1,17 +1,21 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super();
-  }
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  private connected = false;
 
-  async onModuleInit() {
-    await this.$connect(); // Connect to the database when the module initializes
+  async connectIfNeeded() {
+    if (!this.connected) {
+      await this.$connect();
+      this.connected = true;
+    }
   }
 
   async onModuleDestroy() {
-    await this.$disconnect(); // Disconnect from the database when the module is destroyed
+    if (this.connected) {
+      await this.$disconnect();
+      this.connected = false;
+    }
   }
 }
