0813770c2740cbbc1d1d85b8937ccf53b36b4a55
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2025-04-12 17:20:35 +0700

    fe: create folder event-management

---
+++ frontend/src/app/(admin)/admin/event-management/components/eventPage.tsx
@@ -0,0 +1,14 @@
+/* Package System */
+import 'tailwindcss/tailwind.css';
+
+/* Package Application */
+
+
+export default function EventPage() {
+    return (
+        <>
+            <h1 className="text-2xl font-bold text-[#0C4762] mb-1">Quản lý Sự kiện</h1>
+            <div className="border-t-2 border-[#0C4762] mt-2"></div>            
+        </>
+    )
+}
