5b9e7acad51c0b318a53f463b4ec510d876b532b
DatTruong0108 <dattantruong.work@gmail.com>
2025-04-17 23:59:06 +0700

    FE: add chatbox btn to layout

---
+++ frontend/src/app/(chatbox)/components/chatboxBtn.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+/* Package System */
+import Image from 'next/image';
+import { useState } from 'react';
+
+/* Package Application */
+import '@/styles/admin/chatbox.css';
+
+const logoPath = '/images/chatbox-space1.png';
+
+export default function ChatboxButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenChatbox = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <button 
+        className="chat-box-btn flex items-center justify-center"
+        onClick={handleOpenChatbox}
+      >
+        <Image src={logoPath} alt='logo chatbox' height={56} width={71.3} />
+        <span className="chat-box-name">Eve Chatbox</span>
+      </button>
+      <div className={`chat-box-wrapper ${isOpen ? 'open' : ''}`}>
+        
+      </div>
+    </>
+  )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,18 @@
+import ChatboxButton from "./(chatbox)/components/chatboxBtn";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <>
+            <div className="tt-content relative">
+              <div>
+                {children}
+              </div>
+              <ChatboxButton />
+            </div>
+          </>
+        </body>
       </html>
     );
   }
