592e4fbacec20d1b142b2cd49a6d67cdb945b237
dttHuongg <huong.dinhtthuy203@gmail.com>
2025-04-18 15:55:35 +0700

    fea: ui change password

---
+++ frontend/src/app/(account-management)/change-password/layout.tsx
@@ -0,0 +1,19 @@
+import NavigationBar from "@/app/(dashboard)/components/common/navigationBar"
+import DashboardLoading from "@/app/(dashboard)/loading"
+import { Providers } from "@/app/provider"
+import { Suspense } from "react"
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Providers>
+      <Suspense fallback={<DashboardLoading />}>
+        <NavigationBar/>
+        <main>{children}</main>
+      </Suspense>
+    </Providers>  
+  )
+}
