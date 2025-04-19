35cbe732e07f794fddd6de7f5a2319261d0bf1a9
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2025-04-19 16:28:42 +0700

    create Showing page

---
+++ frontend/src/app/(admin)/admin/showing-management/components/showingPage.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+/* Package System */
+import 'tailwindcss/tailwind.css';
+
+
+/* Package Application */
+
+
+export default function ShowingPage() {
+    return (
+        <>
+            <h1 className="text-2xl font-bold text-[#0C4762] mb-1">Quản lý Showing</h1>
+            <div className="border-t-2 border-[#0C4762] mt-2"></div>
+
+           
+        </>
+    )
+}
