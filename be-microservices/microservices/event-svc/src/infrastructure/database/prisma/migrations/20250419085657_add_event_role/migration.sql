39491f4274c3a4e98e082c60213bc763e47e1c76
21120185 <21120185@student.hcmus.edu.vn>
2025-04-20 12:42:32 +0700

    DB: define data model for event role

---
+++ be-microservices/microservices/event-svc/src/infrastructure/database/prisma/migrations/20250419085657_add_event_role/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "event_role" (
+    "id" INTEGER NOT NULL,
+    "isEdited" BOOLEAN NOT NULL,
+    "isSummarized" BOOLEAN NOT NULL,
+    "viewVoucher" BOOLEAN NOT NULL,
+    "marketing" BOOLEAN NOT NULL,
+    "viewOrder" BOOLEAN NOT NULL,
+    "viewSeatmap" BOOLEAN NOT NULL,
+    "viewMember" BOOLEAN NOT NULL,
+    "checkin" BOOLEAN NOT NULL,
+    "checkout" BOOLEAN NOT NULL,
+    "redeem" BOOLEAN NOT NULL,
+
+    CONSTRAINT "event_role_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "EventUserRelationship" ADD CONSTRAINT "EventUserRelationship_role_fkey" FOREIGN KEY ("role") REFERENCES "event_role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
