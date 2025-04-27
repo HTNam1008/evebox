1a8f2e2fa46c86d0da5ff1d78da59b097f8dcdf9
DatTruong0108 <dattantruong.work@gmail.com>
2025-04-27 20:30:27 +0700

    fix: delete html and providers in sub layout

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from "react"
-import Footer from "./components/common/footer"
-import NavigationBar from "./components/common/navigationBar"
 import DashboardLoading from "./loading"
-import { Providers } from "../provider"
 
 export default function DefaultLayout({
   children,
@@ -10,12 +7,10 @@ export default function DefaultLayout({
   children: React.ReactNode
 }) {
   return (
-    <Providers>
-      <Suspense fallback={<DashboardLoading />}>
-        {/* <NavigationBar /> */}
-        <main>{children}</main>
-        {/* <Footer /> */}
-      </Suspense>
-    </Providers>
+    <Suspense fallback={<DashboardLoading />}>
+      {/* <NavigationBar /> */}
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </Suspense>
   )
 }
