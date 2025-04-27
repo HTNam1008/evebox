661911b5d0099bf9a43ac6590c625a0577c3f42b
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2025-04-27 23:17:36 +0700

    fea: done legal-document of business

--- frontend/src/app/(organizer)/organizer/legal-document/business/page.tsx
+++ frontend/src/app/(organizer)/organizer/legal-document/business/page.tsx
@@ -3,7 +3,7 @@
 
 /* Package Application */
 import 'tailwindcss/tailwind.css';
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { useRouter } from 'next/navigation';
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
@@ -11,34 +11,36 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { ArrowLeft } from "lucide-react"
 
-/* Package System */
-import Sidebar from '../../components/sidebar';
-
 export default function PdfViewerPage() {
     const router = useRouter();
     const [isClient, setIsClient] = useState(false);
-    // const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+    const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+    const [fileName, setFileName] = useState<string | null>(null);
+    const [originName, setOriginName] = useState<string | null>("");
 
     useEffect(() => {
         setIsClient(true);
 
         // Gọi API lấy đường dẫn file PDF hiện tại
-        // fetch('/api/get-current-pdf')
-        //     .then((res) => res.json())
-        //     .then((data) => {
-        //         setPdfUrl(data.url);
-        //     });
+        setPdfUrl("https://raw.githubusercontent.com/huethanh-2805/term-evebox/main/6.%20Evebox_Danh_muc_hang_hoa_dich_cam_kinh_doanh_va_kinh_doanh_co_dieu_kien.pdf")
     }, []);
 
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-    // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     const file = e.target.files?.[0];
-    //     if (file) {
-    //         const fileUrl = URL.createObjectURL(file);
-    //         setPdfUrl(fileUrl);
-    //     }
-    // };
+    // Nút thay đổi content -> call api để upload
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            // Tạo URL cho file PDF mới
+            const fileUrl = URL.createObjectURL(file);
+            setPdfUrl(fileUrl);
+
+            setOriginName(file.name);
+            const maxLength = 25;    // Giới hạn tên file hiển thị (giới hạn 30 ký tự)
+            const truncatedFileName = file.name.length > maxLength ? `${file.name.substring(0, maxLength)}...` : file.name;
+            setFileName(truncatedFileName); 
+        }
+    };
 
     return isClient ? (
         <div>
@@ -49,11 +51,23 @@ export default function PdfViewerPage() {
 
             <div className="border-t-2 border-[#0C4762] mt-2 mb-6"></div>
 
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
-                <div style={{ height: "100vh" }}>
-                    <Viewer fileUrl="/pdf/example.pdf" plugins={[defaultLayoutPluginInstance]} />
-                </div>
-            </Worker>
+            {/* Nút thay đổi content */}
+            <div className="change-content-btn mb-6">
+                <label title={originName || "Chọn file PDF"} htmlFor="pdf-upload" className="bg-[#51DACF] text-[#0C4762] px-6 py-3 rounded-lg cursor-pointer hover:bg-[#085a73] hover:text-white transition-colors duration-200">
+                    {fileName ? `Đã chọn file: ${fileName}` : "Chọn File PDF"}
+                </label>
+                <input className="hidden" id="pdf-upload" type="file" accept=".pdf" onChange={handleFileChange} />
+            </div>
+
+            {pdfUrl ? (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
+                    <div style={{ height: "100vh" }}>
+                        <Viewer fileUrl={pdfUrl} plugins={[defaultLayoutPluginInstance]} />
+                    </div>
+                </Worker>
+            ) : (
+                <p>Loading PDF...</p>
+            )}
         </div>
     ) : (
         <p>Loading...</p>
