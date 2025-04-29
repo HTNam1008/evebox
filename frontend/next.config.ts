033642ae4a4e578de01c4f22e43009a2a2f4d6c3
21120185 <21120185@student.hcmus.edu.vn>
2025-04-29 17:13:55 +0700

    fea: implement and call API post click when click to event detail

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["images.tkbcdn.com", "salt.tkbcdn.com", "res.cloudinary.com"], // Add all external domains here
   },
