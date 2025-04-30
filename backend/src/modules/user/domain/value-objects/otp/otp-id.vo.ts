c89cecf71e5e9b6fb42180af5318a4021925cdc0
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-04-30 12:41:15 +0700

    Delete Backend Folder

--- backend/src/modules/user/domain/value-objects/otp/otp-id.vo.ts
+++
@@ -1,8 +0,0 @@
-import { Identifier } from 'src/libs/ddd/identifier.base';
-import { v4 as uuidv4 } from 'uuid';
-
-export class OTPId extends Identifier<string> {
-  static generate(): OTPId {
-    return new OTPId(uuidv4());
-  }
-}
