ff064de84c0c1e6c1cbf61c7b07e2bf02387a3f0
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2025-05-01 16:09:45 +0700

    fix: add handle error page and split function handleFileChange into component

--- frontend/src/app/(dashboard)/(term)/dispute-settlement-policy/page.tsx
+++ frontend/src/app/(dashboard)/(term)/dispute-settlement-policy/page.tsx
@@ -1,45 +1,51 @@
-
 "use client";
 
 /* Package System */
 import 'tailwindcss/tailwind.css';
 import { useEffect, useState, ChangeEvent } from "react";
-import { useRouter } from 'next/navigation';
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { ArrowLeft } from "lucide-react"
+
+/* Package Application */
+import { handleFileChange as handleFileChangeUtil } from '@/app/(dashboard)/libs/functions/handleFileChange';
 
 export default function PdfViewerPage() {
-    const router = useRouter();
     const [isClient, setIsClient] = useState(false);
     const [pdfUrl, setPdfUrl] = useState<string | null>(null);
     const [fileName, setFileName] = useState<string | null>(null);
     const [originName, setOriginName] = useState<string | null>("");
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         setIsClient(true);
 
         // Gọi API lấy đường dẫn file PDF hiện tại
-        setPdfUrl("https://raw.githubusercontent.com/huethanh-2805/term-evebox/main/Co-che-giai-quyet-tranh-chap-khieu-nai.pdf")
+        const url = "https://raw.githubusercontent.com/huethanh-2805/term-evebox/main/Co-che-giai-quyet-tranh-chap-khieu-nai.pdf";
+        
+        const checkPdf = async () => {
+            try {
+                const res = await fetch(url, { method: 'HEAD' });
+                if (!res.ok) {
+                    throw new Error("Không thể tải PDF từ URL.");
+                }
+                setPdfUrl(url);
+            } catch (err) {
+                setError(err as Error);
+            }
+        };
+
+        checkPdf();
     }, []);
+
+    if (error) throw error;
 
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
     // Nút thay đổi content -> call api để upload
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (file) {
-            // Tạo URL cho file PDF mới
-            const fileUrl = URL.createObjectURL(file);
-            setPdfUrl(fileUrl);
-
-            setOriginName(file.name);
-            const maxLength = 25;    // Giới hạn tên file hiển thị (giới hạn 30 ký tự)
-            const truncatedFileName = file.name.length > maxLength ? `${file.name.substring(0, maxLength)}...` : file.name;
-            setFileName(truncatedFileName); 
-        }
+        handleFileChangeUtil(e, setPdfUrl, setFileName, setOriginName, setError);
     };
 
     return isClient ? (
