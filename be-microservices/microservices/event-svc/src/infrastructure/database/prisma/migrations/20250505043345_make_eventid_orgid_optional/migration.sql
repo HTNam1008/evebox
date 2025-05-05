d40a0b4a0ec65d82f1e3febdce0862021804416b
21120185 <21120185@student.hcmus.edu.vn>
2025-05-06 00:08:06 +0700

    DB: synce with ticket-svc and rag-svc

--- be-microservices/microservices/event-svc/src/infrastructure/database/prisma/migrations/20250505043345_make_eventid_orgid_optional/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "favorite_noti_history" DROP CONSTRAINT "favorite_noti_history_eventId_fkey";
-
--- AlterTable
-ALTER TABLE "favorite_noti_history" ALTER COLUMN "orgId" DROP NOT NULL,
-ALTER COLUMN "eventId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "favorite_noti_history" ADD CONSTRAINT "favorite_noti_history_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Events"("id") ON DELETE SET NULL ON UPDATE CASCADE;
